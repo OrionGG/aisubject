11e0a30b7dfc1ccbd5bc9d8b4eb233b59df7e205
oriong2@gmail.com <oriong2@gmail.com@fccf867a-3222-7993-d9f8-d2a13a29b698>
2012-11-30 09:52:46 +0000

    New Console class finished step1

--- CrotalReading/src/CrotalReading/CrotalReadingConsole.java
+++ CrotalReading/src/CrotalReading/CrotalReadingConsole.java
@@ -5,6 +5,7 @@
 package CrotalReading;
 
 import ij.ImagePlus;
+import ij.io.FileSaver;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -13,9 +14,12 @@ import java.util.ArrayList;
  * @author Orion
  */
 public class CrotalReadingConsole {
-    private static int ArgumentNumber = 2;
+    private static int MinimumArgumentNumber = 2;
     private static int ImageFolderIndex = 0;
     private static int NumbersToDetectIndex = 1;
+    private static int OutputFolderIndex = 2;    
+    
+    private static String OutputDefaultFolder = "Output";
     
     private static String IMAGEFOLDEREMPTYERROR = "ERROR Image Folder can no be open: ";
     private static String NUMBERTTODETECTCONVERTERROR = "ERROR Argument does not contain a parsable integer: ";
@@ -25,12 +29,15 @@ public class CrotalReadingConsole {
     /**
      *  args[0] = string with the folder where the images will be
      *  args[1] = integer with the numbers to detect
+     *  args[2] = string with the output folder where the result images will be saved
+     *  args[2] = args[0]/output by default
      * 
      **/
     public static void main(String args[]) throws java.io.IOException{
-        if(ArgumentNumber <= args.length){
-            String sImageFolderIndex = args[ImageFolderIndex];
-            if(sImageFolderIndex != ""){  
+        if(MinimumArgumentNumber <= args.length){
+            String sImageFolder = args[ImageFolderIndex];
+            
+            if(sImageFolder != ""){  
                 int iNumbersToDetect = 0;
                 try{
                     iNumbersToDetect = Integer.parseInt(args[NumbersToDetectIndex]);
@@ -39,7 +46,7 @@ public class CrotalReadingConsole {
                       throw new java.io.IOException(NUMBERTTODETECTCONVERTERROR + args[NumbersToDetectIndex] + ". " + nfe.getMessage());                    
                 }
                 
-                ArrayList<File> aListOfFiles = getFiles(sImageFolderIndex);
+                ArrayList<File> aListOfFiles = getFiles(sImageFolder);
                 
                 for (File oFile : aListOfFiles)
                 {
@@ -48,7 +55,21 @@ public class CrotalReadingConsole {
                     ImagePlus oImagePlus = CrotalReader.VideoVigilanciaProcess(sFileName, iNumbersToDetect);
                     
                     if(oImagePlus != null){
+                        FileSaver oFileSaver = new FileSaver(oImagePlus);
                         
+                        String sOutputFolder = "";
+                        
+                        if(OutputFolderIndex < args.length){
+                            sOutputFolder = args[OutputFolderIndex];
+                            if(sOutputFolder == ""){
+                                sOutputFolder = combine(sImageFolder, OutputDefaultFolder);
+                            }
+                        }
+                        else{
+                            sOutputFolder = combine(sImageFolder, OutputDefaultFolder);
+                        }                        
+                        
+                        oFileSaver.saveAsTiff(combine(sOutputFolder,sFileName));
                     }
                     else{
                         throw new java.io.IOException(IMAGENULLERROR);
@@ -57,7 +78,7 @@ public class CrotalReadingConsole {
                 
             }
             else{
-                throw new java.io.IOException(IMAGEFOLDEREMPTYERROR + sImageFolderIndex);
+                throw new java.io.IOException(IMAGEFOLDEREMPTYERROR + sImageFolder);
             }
             
         }
@@ -81,6 +102,13 @@ public class CrotalReadingConsole {
         }
         
         return aFilesResult;
+    }
+    
+    public static String combine (String path1, String path2)
+    {
+        File file1 = new File(path1);
+        File file2 = new File(file1, path2);
+        return file2.getPath();
     }
     
 }
