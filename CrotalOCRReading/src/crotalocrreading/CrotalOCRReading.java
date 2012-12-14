9735467bc8ebc28d8b4c5eaca6e69978004b7c6d
oriong2@gmail.com <oriong2@gmail.com@fccf867a-3222-7993-d9f8-d2a13a29b698>
2012-12-14 17:19:48 +0000

    Program release

--- CrotalOCRReading/src/crotalocrreading/CrotalOCRReading.java
+++ CrotalOCRReading/src/crotalocrreading/CrotalOCRReading.java
@@ -4,16 +4,38 @@
  */
 package crotalocrreading;
 
+import ij.ImagePlus;
+import ij.io.Opener;
+import java.io.File;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 /**
  *
  * @author Orion
  */
 public class CrotalOCRReading {
+    private static final String IMAGEFILEOPENERROR = "ERROR Image file could not be opened: ";
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String readCrotalImage(String sImagePath){
+        String sResult = "";
+        
+        File oFile = new File(sImagePath);
+        
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+        instance.setDatapath("D:\\Master Vision Artificial\\AI\\Tess4J");
+        
+        try {
+            sResult = instance.doOCR(oFile);
+            System.out.println(sResult);
+            
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+        
+        return sResult;
     }
+    
+    
 }
