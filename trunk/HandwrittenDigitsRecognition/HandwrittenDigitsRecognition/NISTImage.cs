60fffb1cc992e8e7498002aaa23b73dc58c97809
oriong2@gmail.com <oriong2@gmail.com@fccf867a-3222-7993-d9f8-d2a13a29b698>
2012-12-23 18:36:43 +0000

    Added practise 4. Hand written recognition.

---
+++ trunk/HandwrittenDigitsRecognition/HandwrittenDigitsRecognition/NISTImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HandwrittenDigitsRecognition
+{
+    class NISTImage
+    {
+        public static List<byte[,]> Read(string sFileName)
+        {
+            List<byte[,]> oResult = new List<byte[,]>();
+            //Fragmento de código que lee las imágenes
+
+            try
+            {
+                BinaryReader oBinaryReader = new BinaryReader(File.OpenRead(sFileName));
+                int vacio1 = oBinaryReader.ReadInt32();
+                int vacio2 = oBinaryReader.ReadInt32();
+                int tipo = oBinaryReader.ReadInt32();   //Valdrá 8
+                int dim = oBinaryReader.ReadInt32();    //Valdrá 3
+                int numImagenes = oBinaryReader.ReadInt32() << 24 | oBinaryReader.ReadInt32() << 16 | oBinaryReader.ReadInt32() << 8 | oBinaryReader.ReadInt32();   //Valdrá 60000
+                int ancho = oBinaryReader.ReadInt32() << 24 | oBinaryReader.ReadInt32() << 16 | oBinaryReader.ReadInt32() << 8 | oBinaryReader.ReadInt32();         //Valdrá 28
+                int alto = oBinaryReader.ReadInt32() << 24 | oBinaryReader.ReadInt32() << 16 | oBinaryReader.ReadInt32() << 8 | oBinaryReader.ReadInt32();          //Valdra 28
+
+                //Este bucle lee todas las imágenes
+                for (int c = 0; c < numImagenes; c++)
+                {
+                    byte[,] imagen = new byte[ancho, alto];
+                    for (int y = 0; y < alto; y++)
+                        for (int x = 0; x < ancho; x++)
+                        {
+                            int punto = oBinaryReader.ReadInt32();
+                            imagen[x, y] = (byte)punto;
+                        }
+                    //Aqui ya tenemos la imagen c-esima leída
+                    oResult.Add(imagen);
+                }
+            }
+            catch (IOException ex)
+            {
+            }
+
+            return oResult;
+        }
+    }
+}
---
+++ trunk/HandwrittenDigitsRecognition/HandwrittenDigitsRecognition/NISTLabel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HandwrittenDigitsRecognition
+{
+    class NISTLabel
+    {
+        public static List<int> Read(string sFileName)
+        {
+            List<int> lResutl = new List<int>();
+            //Fragmento de código que lee las etiquetas correspondientes a cada imagen
+
+            try
+            {
+                BinaryReader oBinaryReader = new BinaryReader(File.OpenRead(sFileName));
+                int vacio1 = oBinaryReader.ReadInt32();
+                int vacio2 = oBinaryReader.ReadInt32();
+                int tipo = oBinaryReader.ReadInt32();  //Valdrá 8
+                int dim = oBinaryReader.ReadInt32();   //Valdrá 1
+                int numEtiquetas = oBinaryReader.ReadInt32() << 24 | oBinaryReader.ReadInt32() << 16 | oBinaryReader.ReadInt32() << 8 | oBinaryReader.ReadInt32();   //Valdrá 60000
+
+                //Este bucle lee todas las etiquetas
+                for (int c = 0; c < numEtiquetas; c++)
+                {
+                    int etiqueta = oBinaryReader.ReadInt32();
+                    //Aqui ya tenemos la etiqueta de la imagen c-esima leída
+                    lResutl.Add(etiqueta);
+
+                }
+            }
+            catch (IOException ex)
+            {
+            }
+
+            return lResutl;
+        }
+    }
+}
